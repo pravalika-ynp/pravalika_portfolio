1c3eb1424c5b1a7bad8b8524eec891b8dc1b9b0e
pravalika-ynp <125171828+pravalika-ynp@users.noreply.github.com>
2024-07-22 19:28:07 +0530

    node.js

---
+++ node.js
@@ -0,0 +1,30 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
+  
+  document.getElementById('image-upload-1').addEventListener('change', function(event) {
+    previewImage(event, 'image-preview-1');
+  });
+  
+  document.getElementById('image-upload-2').addEventListener('change', function(event) {
+    previewImage(event, 'image-preview-2');
+  });
+  
+  document.getElementById('image-upload-3').addEventListener('change', function(event) {
+    previewImage(event, 'image-preview-3');
+  });
+  
+  document.getElementById('image-upload-4').addEventListener('change', function(event) {
+    previewImage(event, 'image-preview-4');
+  });
+  
+  function previewImage(event, previewId) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const imgElement = document.getElementById(previewId);
+      imgElement.src = e.target.result;
+      imgElement.classList.remove('hidden');
+    };
+    reader.readAsDataURL(file);
+  }
